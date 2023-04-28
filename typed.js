9f964cf1c5de9ab718c65977d0df655d96ee9e24
Lavin-tom <lavintom007@gmail.com>
2023-04-28 15:46:40 +0530

    deleted private folder

--- typed.js
+++ typed.js
@@ -1,5 +1,19 @@
+var hour = new Date().getHours();
+var message = "";
+
+if (hour >= 5 && hour < 12) {
+  message = "Good morning!";
+} else if (hour >= 12 && hour < 18) {
+  message = "Good afternoon!";
+} else if (hour >= 18 && hour < 22) {
+  message = "Good evening!";
+} else {
+  message = "Good night!";
+}
+
 var typed = new Typed('#typed-text', {
-    strings: ["web page is under testing!","hello world"],
-    typeSpeed: 100,
-    loop: true
-  });
+  strings: [message+" Boss"],
+  typeSpeed: 100,
+  loop: true,
+  showCursor: false
+});
