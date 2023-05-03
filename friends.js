93c64d52143d9ce2e3037791c9a0bafb0b95f89e
Lavin-tom <lavintom007@gmail.com>
2023-05-03 15:58:26 +0530

    modify data-base and warning

--- friends.js
+++ friends.js
@@ -1,24 +1,111 @@
 const friends = [
-    //may
-    {
-        name: 'Arun',
-        birthday: new Date('May 2, 1997'),
+    {//may
+      name: 'Arun',
+      birthday: new Date('May 2, 1997'),
     },
     {
-        name: 'Ebin',
-        birthday: new Date('May 2, 1997'),
+      name: 'Ebin',
+      birthday: new Date('May 2, 1997'),
     },
     {
       name: 'Sajan',
       birthday: new Date('May 4, 1985'),
     },
     {
-        name: 'Vaishna',
-        birthday: new Date('May 5, 1997'),
+      name: 'Vaishna',
+      birthday: new Date('May 5, 1997'),
     },
     {
       name: 'Athulya',
       birthday: new Date('May 6, 1997'),
+    },
+    {
+      name: 'Akhil',
+      birthday: new Date('May 7, 1997'),
+    },
+    {
+      name: 'Arjun',
+      birthday: new Date('May 7, 1997'),
+    },
+    {
+      name: 'Digina',
+      birthday: new Date('May 13, 1997'),
+    },
+    {
+      name: 'Neethu',
+      birthday: new Date('May 13, 1997'),
+    },
+    {
+      name: 'Yadhu',
+      birthday: new Date('May 15, 1997'),
+    },
+    {
+      name: 'Alvin',
+      birthday: new Date('May 16, 1997'),
+    },
+    {
+      name: 'Emil',
+      birthday: new Date('May 17, 1997'),
+    },
+    {
+      name: 'Merlin',
+      birthday: new Date('May 18, 1997'),
+    },
+    {
+      name: 'Nimisha',
+      birthday: new Date('May 18, 1997'),
+    },
+    {
+      name: 'Vimal',
+      birthday: new Date('May 20, 1997'),
+    },
+    {
+      name: 'Bibin',
+      birthday: new Date('May 25, 1997'),
+    },
+    {
+      name: 'Nandhu',
+      birthday: new Date('May 25, 1997'),
+    },
+    {//june
+      name: 'Jithu',
+      birthday: new Date('June 3, 1997'),
+    },
+    {
+      name: 'Rakhil',
+      birthday: new Date('June 8, 1997'),
+    },
+    {
+      name: 'Aslam',
+      birthday: new Date('June 13, 1997'),
+    },
+    {
+      name: 'Jobin',
+      birthday: new Date('June 17, 1997'),
+    },
+    {
+      name: 'Kavyasree',
+      birthday: new Date('June 18, 1997'),
+    },
+    {
+      name: 'Dilsha',
+      birthday: new Date('June 19, 1997'),
+    },
+    {
+      name: 'Linu',
+      birthday: new Date('June 20, 1997'),
+    },
+    {
+      name: 'Soubhagya',
+      birthday: new Date('June 25, 1997'),
+    },
+    {
+      name: 'Abin',
+      birthday: new Date('June 26, 1997'),
+    },
+    {
+      name: 'Vaishnavi',
+      birthday: new Date('June 30, 1997'),
     }
   ];
   
