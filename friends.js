e3a6040b57fecdf68283d2118cbccdf995fe8635
LAVIN TOM K ABRAHAM <53207510+Lavin-tom@users.noreply.github.com>
2023-05-08 09:39:28 +0530

    Update friends.js
    added more data to the DB

--- friends.js
+++ friends.js
@@ -1,23 +1,23 @@
 const friends = [
     {//may
       name: 'Arun',
-      birthday: new Date('May 2, 1997'),
+      birthday: new Date('May 2, 0000'),
     },
     {
       name: 'Ebin',
-      birthday: new Date('May 2, 1997'),
+      birthday: new Date('May 2, 0000'),
     },
     {
       name: 'Sajan',
-      birthday: new Date('May 4, 1985'),
+      birthday: new Date('May 4, 0000'),
     },
     {
       name: 'Vaishna',
-      birthday: new Date('May 5, 1997'),
+      birthday: new Date('May 5, 0000'),
     },
     {
       name: 'Athulya',
-      birthday: new Date('May 6, 1997'),
+      birthday: new Date('May 6, 0000'),
     },
     {
       name: 'Akhil',
@@ -29,222 +29,354 @@ const friends = [
     },
     {
       name: 'Digina',
-      birthday: new Date('May 13, 1997'),
+      birthday: new Date('May 13, 0000'),
     },
     {
       name: 'Neethu',
-      birthday: new Date('May 13, 1997'),
+      birthday: new Date('May 13, 0000'),
     },
     {
       name: 'Yadhu',
-      birthday: new Date('May 15, 1997'),
+      birthday: new Date('May 15, 0000'),
     },
     {
       name: 'Alvin',
-      birthday: new Date('May 16, 1997'),
+      birthday: new Date('May 16, 0000'),
     },
     {
       name: 'Emil',
-      birthday: new Date('May 17, 1997'),
+      birthday: new Date('May 17, 0000'),
     },
     {
       name: 'Merlin',
-      birthday: new Date('May 18, 1997'),
+      birthday: new Date('May 18, 0000'),
     },
     {
       name: 'Nimisha',
-      birthday: new Date('May 18, 1997'),
+      birthday: new Date('May 18, 0000'),
     },
     {
       name: 'Vimal',
-      birthday: new Date('May 20, 1997'),
+      birthday: new Date('May 20, 0000'),
     },
     {
       name: 'Bibin',
-      birthday: new Date('May 25, 1997'),
+      birthday: new Date('May 25, 0000'),
     },
     {
       name: 'Nandhu',
-      birthday: new Date('May 25, 1997'),
+      birthday: new Date('May 25, 0000'),
     },
     {//june
       name: 'Jithu',
-      birthday: new Date('June 3, 1997'),
+      birthday: new Date('June 3, 0000'),
     },
     {
       name: 'Rakhil',
-      birthday: new Date('June 8, 1997'),
+      birthday: new Date('June 8, 0000'),
     },
     {
       name: 'Aslam',
-      birthday: new Date('June 13, 1997'),
+      birthday: new Date('June 13, 0000'),
     },
     {
       name: 'Jobin',
-      birthday: new Date('June 17, 1997'),
+      birthday: new Date('June 17, 0000'),
     },
     {
       name: 'Kavyasree',
-      birthday: new Date('June 18, 1997'),
+      birthday: new Date('June 18, 0000'),
     },
     {
       name: 'Dilsha',
-      birthday: new Date('June 19, 1997'),
+      birthday: new Date('June 19, 0000'),
     },
     {
       name: 'Linu',
-      birthday: new Date('June 20, 1997'),
+      birthday: new Date('June 20, 0000'),
     },
     {
       name: 'Soubhagya',
-      birthday: new Date('June 25, 1997'),
+      birthday: new Date('June 25, 0000'),
     },
     {
       name: 'Abin',
-      birthday: new Date('June 26, 1997'),
+      birthday: new Date('June 26, 0000'),
     },
     {
       name: 'Vaishnavi',
-      birthday: new Date('June 30, 1997'),
+      birthday: new Date('June 30, 0000'),
     },
     {//july
       name: 'Nikhil',
-      birthday: new Date('July 3, 1997'),
+      birthday: new Date('July 3, 0000'),
     },
     {
       name: 'Midhun',
-      birthday: new Date('July 4, 1997'),
+      birthday: new Date('July 4, 0000'),
     },
     {
       name: 'Sunil',
-      birthday: new Date('July 7, 1997'),
+      birthday: new Date('July 7, 0000'),
     },
     {
       name: 'Sagar',
-      birthday: new Date('July 11, 1997'),
+      birthday: new Date('July 11, 0000'),
     },    
     {
       name: 'Jyothy',
-      birthday: new Date('July 15, 1997'),
+      birthday: new Date('July 15, 0000'),
     },
     {
       name: 'Amma',
-      birthday: new Date('July 21, 1997'),
+      birthday: new Date('July 21, 0000'),
     },
     {
       name: 'Anna',
-      birthday: new Date('July 22, 1997'),
+      birthday: new Date('July 22, 0000'),
     },
     {
       name: 'Sandra',
-      birthday: new Date('July 23, 1997'),
+      birthday: new Date('July 23, 0000'),
     },
     {
       name: 'Athul',
-      birthday: new Date('July 27, 1997'),
+      birthday: new Date('July 27, 0000'),
     },
     {
       name: 'Thoma',
-      birthday: new Date('July 30, 1997'),
+      birthday: new Date('July 30, 0000'),
     },
     {//August
       name: 'Adarsh',
-      birthday: new Date('August 1, 1997'),
+      birthday: new Date('August 1, 0000'),
     },
     {
       name: 'Sukanya',
-      birthday: new Date('August 1, 1997'),
+      birthday: new Date('August 1, 0000'),
     },
     {
       name: 'Jaidon',
-      birthday: new Date('August 5, 1997'),
+      birthday: new Date('August 5, 0000'),
     },
     {
       name: 'Anjali',
-      birthday: new Date('August 7, 1997'),
+      birthday: new Date('August 7, 0000'),
     },
     {
       name: 'Gokul',
-      birthday: new Date('August 8, 1997'),
+      birthday: new Date('August 8, 0000'),
     },
     {
       name: 'Sarika',
-      birthday: new Date('August 9, 1997'),
+      birthday: new Date('August 9, 0000'),
     },
     {
       name: 'Jerin',
-      birthday: new Date('August 12, 1997'),
+      birthday: new Date('August 12, 0000'),
     },
     {
       name: 'Saran',
-      birthday: new Date('August 15, 1997'),
+      birthday: new Date('August 15, 0000'),
     },
     {
       name: 'Binil',
-      birthday: new Date('August 18, 1997'),
+      birthday: new Date('August 18, 0000'),
     },
     {
       name: 'Boss',
-      birthday: new Date('August 18, 1997'),
+      birthday: new Date('August 18, 0000'),
     },
     {
       name: 'Namitha',
-      birthday: new Date('August 28, 1997'),
+      birthday: new Date('August 28, 0000'),
     },
     {
       name: 'Greshma',
-      birthday: new Date('August 29, 1997'),
+      birthday: new Date('August 29, 0000'),
     },
     {//septempber
       name: 'Greena',
-      birthday: new Date('September 1, 1997'),
+      birthday: new Date('September 1, 0000'),
     },
     {
       name: 'Sivapriya',
-      birthday: new Date('September 10, 1997'),
+      birthday: new Date('September 10, 0000'),
     },
     {
       name: 'Rijo',
-      birthday: new Date('September 13, 1997'),
+      birthday: new Date('September 13, 0000'),
     },
     {
       name: 'Gokul',
-      birthday: new Date('September 17, 1997'),
+      birthday: new Date('September 17, 0000'),
     },
     {
       name: 'Jeswin',
-      birthday: new Date('September 18, 1997'),
+      birthday: new Date('September 18, 0000'),
     },
     {
       name: 'Vinitha',
-      birthday: new Date('September 20, 1997'),
+      birthday: new Date('September 20, 0000'),
     },
     {
       name: 'Jucia',
-      birthday: new Date('September 20, 1997'),
+      birthday: new Date('September 20, 0000'),
     },
     {
       name: 'Gayathri',
-      birthday: new Date('September 22, 1997'),
+      birthday: new Date('September 22, 0000'),
     },
     {
       name: 'Vishnu',
-      birthday: new Date('September 22, 1997'),
+      birthday: new Date('September 22, 0000'),
     },
     {
       name: 'Tom',
-      birthday: new Date('September 24, 1997'),
+      birthday: new Date('September 24, 0000'),
     },
     {
       name: 'Ananthakrishnan',
-      birthday: new Date('September 27, 1997'),
+      birthday: new Date('September 27, 0000'),
     },
     {
       name: 'Sethu',
-      birthday: new Date('September 29, 1997'),
+      birthday: new Date('September 29, 0000'),
     },
     {
       name: 'Swathy',
-      birthday: new Date('September 1, 1997'),
+      birthday: new Date('September 1, 0000'),
+    },
+    {//Oct
+      name: 'Bilgy',
+      birthday: new Date('October 4, 0000'),
+    },
+    {
+      name: 'Greeshma',
+      birthday: new Date('October 12, 0000'),
+    },
+    {
+      name: 'Albin',
+      birthday: new Date('October 14, 0000'),
+    },
+    {
+      name: 'Neethish',
+      birthday: new Date('October 17, 0000'),
+    },
+    {
+      name: 'Sruthy',
+      birthday: new Date('October 17, 0000'),
+    },
+    {
+      name: 'Bincy',
+      birthday: new Date('October 21, 0000'),
+    },
+    {
+      name: 'Sinto',
+      birthday: new Date('October 21, 0000'),
+    },
+    {
+      name: 'Aju',
+      birthday: new Date('October 22, 0000'),
+    },
+    {
+      name: 'Maneesha',
+      birthday: new Date('October 26, 0000'),
+    },
+    {
+      name: 'Jayke',
+      birthday: new Date('October 27, 0000'),
+    },
+    {
+      name: 'Avinash',
+      birthday: new Date('October 28, 0000'),
+    },
+    {
+      name: 'Vishnu',
+      birthday: new Date('October 29, 0000'),
+    },
+    {
+      name: 'Tissy',
+      birthday: new Date('October 30, 0000'),
+    },
+    {
+      name: 'Aima',
+      birthday: new Date('October 31, 0000'),
+    },
+    {
+      name: 'Judit',
+      birthday: new Date('October 31, 0000'),
+    },
+    {//Nov
+      name: 'Harsha',
+      birthday: new Date('November 04, 0000'),
+    },
+    {
+      name: 'Vijitha',
+      birthday: new Date('November 04, 0000'),
+    },
+    {
+      name: 'Yuvaraja',
+      birthday: new Date('November 10, 0000'),
+    },
+    {
+      name: 'Sarang',
+      birthday: new Date('November 11, 0000'),
+    },
+    {
+      name: 'Georgy',
+      birthday: new Date('November 14, 0000'),
+    },
+    {
+      name: 'Lanwin',
+      birthday: new Date('November 17, 0000'),
+    },
+    {
+      name: 'Mahesh',
+      birthday: new Date('November 22, 0000'),
+    },
+    {
+      name: 'Christy',
+      birthday: new Date('November 25, 0000'),
+    },
+    {
+      name: 'Sudhev',
+      birthday: new Date('November 25, 0000'),
+    },
+    {
+      name: 'Unni',
+      birthday: new Date('November 25, 0000'),
+    },
+    {
+      name: 'Hari',
+      birthday: new Date('November 26, 0000'),
+    },
+    {
+      name: 'Anjali',
+      birthday: new Date('November 27, 0000'),
+    },
+    {
+      name: 'Christin',
+      birthday: new Date('November 27, 0000'),
+    },
+    {
+      name: 'Jojal',
+      birthday: new Date('November 27, 0000'),
+    },
+    {
+      name: 'Nevin',
+      birthday: new Date('November 27, 0000'),
+    },
+    {
+      name: 'Rinto',
+      birthday: new Date('November 27, 0000'),
+    },
+    {
+      name: 'Harikrishnan',
+      birthday: new Date('November 29, 0000'),
+    },
+    {
+      name: 'Bini',
+      birthday: new Date('November 30, 0000'),
     }
   ];
