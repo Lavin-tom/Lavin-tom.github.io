00e74e5a2823278f90b0fe30b4b1850e3e4fafc5
LAVIN TOM K ABRAHAM <53207510+Lavin-tom@users.noreply.github.com>
2023-05-08 10:18:23 +0530

    Update friends.js

--- friends.js
+++ friends.js
@@ -1,4 +1,228 @@
 const friends = [
+    {//Jan
+      name: 'Athimi',
+      birthday: new Date('January 01, 0000'),
+    },    
+    {
+      name: 'Nisarga',
+      birthday: new Date('January 01, 0000'),
+    },
+    {
+      name: 'Ceby',
+      birthday: new Date('January 03, 0000'),
+    },
+    {
+      name: 'Soju',
+      birthday: new Date('January 05, 0000'),
+    },
+    {
+      name: 'Deepu sir',
+      birthday: new Date('January 06, 0000'),
+    },
+    {
+      name: 'Joan',
+      birthday: new Date('January 06, 0000'),
+    },
+    {
+      name: 'Vamsi',
+      birthday: new Date('January 07, 0000'),
+    },
+    {
+      name: 'Santhosh',
+      birthday: new Date('January 13, 0000'),
+    },
+    {
+      name: 'Dinil',
+      birthday: new Date('January 19, 0000'),
+    },
+    {
+      name: 'Melwin',
+      birthday: new Date('January 21, 0000'),
+    },
+    {
+      name: 'Sichu',
+      birthday: new Date('January 22, 0000'),
+    },
+    {
+      name: 'Dains',
+      birthday: new Date('January 23, 0000'),
+    },
+    {//Feb
+      name: 'Johaan',
+      birthday: new Date('February 01, 0000'),
+    },
+    {
+      name: 'Sabin',
+      birthday: new Date('February 02, 0000'),
+    },
+    {
+      name: 'Vineetha',
+      birthday: new Date('February 04, 0000'),
+    },
+    {
+      name: 'Sukrutha',
+      birthday: new Date('February 08, 0000'),
+    },
+    {
+      name: 'Sojin',
+      birthday: new Date('February 10, 0000'),
+    },
+    {
+      name: 'Joel',
+      birthday: new Date('February 11, 0000'),
+    },
+    {
+      name: 'Norah',
+      birthday: new Date('February 11, 0000'),
+    },
+    {
+      name: 'Albin',
+      birthday: new Date('February 13, 0000'),
+    },
+    {
+      name: 'Athira',
+      birthday: new Date('February 13, 0000'),
+    },
+    {
+      name: 'Manu',
+      birthday: new Date('February 16, 0000'),
+    },
+    {
+      name: 'Kelvin',
+      birthday: new Date('February 17, 0000'),
+    },
+    {
+      name: 'Jiswin',
+      birthday: new Date('February 26, 0000'),
+    },
+    {
+      name: 'Joma',
+      birthday: new Date('February 28, 0000'),
+    },
+    {//Mar
+      name: 'Berly',
+      birthday: new Date('March 02, 0000'),
+    },
+    {
+      name: 'Shibhani',
+      birthday: new Date('March 04, 0000'),
+    },
+    {
+      name: 'Selan',
+      birthday: new Date('March 06, 0000'),
+    },
+    {
+      name: 'Tinu',
+      birthday: new Date('March 06, 0000'),
+    },
+    {
+      name: 'Vinayan',
+      birthday: new Date('March 08, 0000'),
+    },
+    {
+      name: 'Michael',
+      birthday: new Date('March 09, 0000'),
+    },
+    {
+      name: 'Achachan',
+      birthday: new Date('March 13, 0000'),
+    },
+    {
+      name: 'Albin
+      birthday: new Date('March 13, 0000'),
+    },
+    {
+      name: 'Ann',
+      birthday: new Date('March 13, 0000'),
+    },
+    {
+      name: 'Anoop',
+      birthday: new Date('March 16, 0000'),
+    },
+    {
+      name: 'Sonu',
+      birthday: new Date('March 17, 0000'),
+    },
+    {
+      name: 'Divya',
+      birthday: new Date('March 19, 0000'),
+    },
+    {
+      name: 'Mobin',
+      birthday: new Date('March 20, 0000'),
+    },
+    {
+      name: 'Slomo',
+      birthday: new Date('March 21, 0000'),
+    },
+    {
+      name: 'Jubina',
+      birthday: new Date('March 23, 0000'),
+    },
+    {
+      name: 'Saiju',
+      birthday: new Date('March 30, 0000'),
+    },
+    {
+      name: 'Sachin',
+      birthday: new Date('March 31, 0000'),
+    },
+    {//April
+      name: 'Johncy',
+      birthday: new Date('April 04, 0000'),
+    },
+    {
+      name: 'Shreyas',
+      birthday: new Date('April 07, 0000'),
+    },
+    {
+      name: 'Joseph',
+      birthday: new Date('April 08, 0000'),
+    },
+    {
+      name: 'Bobby',
+      birthday: new Date('April 09, 0000'),
+    },
+    {
+      name: 'Dennis',
+      birthday: new Date('April 10, 0000'),
+    },
+    {
+      name: 'Stella',
+      birthday: new Date('April 12, 0000'),
+    },
+    {
+      name: 'Bijil',
+      birthday: new Date('April 12, 0000'),
+    },
+    {
+      name: 'Vineetha',
+      birthday: new Date('April 15, 0000'),
+    },
+    {
+      name: 'Tomin',
+      birthday: new Date('April 26, 0000'),
+    },
+    {
+      name: 'Abhirami',
+      birthday: new Date('April 27, 0000'),
+    },
+    {
+      name: 'Vimal',
+      birthday: new Date('April 27, 0000'),
+    },
+    {
+      name: 'Niyas',
+      birthday: new Date('April 28, 0000'),
+    },
+    {
+      name: 'Naveen',
+      birthday: new Date('April 29, 0000'),
+    },
+    {
+      name: 'Shinu',
+      birthday: new Date('April 30, 0000'),
+    },
     {//may
       name: 'Arun',
       birthday: new Date('May 2, 0000'),
@@ -378,5 +602,69 @@ const friends = [
     {
       name: 'Bini',
       birthday: new Date('November 30, 0000'),
+    },
+    {//Dec
+      name: 'Reshma',
+      birthday: new Date('December 02, 0000'),
+    },
+    {
+      name: 'Sobin',
+      birthday: new Date('December 06, 0000'),
+    },
+    {
+      name: 'Merin',
+      birthday: new Date('December 07, 0000'),
+    },
+    {
+      name: 'Rohin',
+      birthday: new Date('December 07, 0000'),
+    },
+    {
+      name: 'Neenu',
+      birthday: new Date('December 11, 0000'),
+    },
+    {
+      name: 'Lavia',
+      birthday: new Date('December 12, 0000'),
+    },
+    {
+      name: 'Archana',
+      birthday: new Date('December 13, 0000'),
+    },
+    {
+      name: 'Albin',
+      birthday: new Date('December 15, 0000'),
+    },
+    {
+      name: 'Joe',
+      birthday: new Date('December 17, 0000'),
+    },
+    {
+      name: 'S.B',
+      birthday: new Date('December 18, 0000'),
+    },
+    {
+      name: 'Justin',
+      birthday: new Date('December 19, 0000'),
+    },
+    {
+      name: 'Robin',
+      birthday: new Date('December 24, 0000'),
+    },
+    {
+      name: 'Sibin',
+      birthday: new Date('December 24, 0000'),
+    },
+    {
+      name: 'Roshan',
+      birthday: new Date('December 25, 0000'),
+    },
+    {
+      name: 'Shijo',
+      birthday: new Date('December 27, 0000'),
+    },
+    {
+      name: 'Athira',
+      birthday: new Date('December 29, 0000'),
     }
   ];
