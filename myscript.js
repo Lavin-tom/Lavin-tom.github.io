8a9cb65a5f8f2d816ac3d855ddaf7b59c7f62999
LAVIN TOM K ABRAHAM <53207510+Lavin-tom@users.noreply.github.com>
2023-05-15 20:03:09 +0530

    Update myscript.js
    optimize particle.js library

--- myscript.js
+++ myscript.js
@@ -179,16 +179,14 @@ var typedName = new Typed('#rand-msg', {
   backSpeed: 0
 });
 
-//particle.js library 
 particlesJS("particles-js", {
   particles: {
-    number: { value: 80, density: { enable: true, value_area: 800 } },
+    number: { value: 30, density: { enable: true, value_area: 800 } },
     color: { value: hexColors[0] },
     shape: {
       type: "circle",
       stroke: { width: 0, color: "#000000" },
-      polygon: { nb_sides: 5 },
-      image: { src: "img/github.svg", width: 100, height: 100 }
+      polygon: { nb_sides: 5 }
     },
     opacity: {
       value: 0.5,
@@ -197,7 +195,7 @@ particlesJS("particles-js", {
     },
     size: {
       value: 3,
-      random: true,
+      random: false,
       anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
     },
     line_linked: {
@@ -220,11 +218,6 @@ particlesJS("particles-js", {
   },
   interactivity: {
     detect_on: "canvas",
-    events: {
-      onhover: { enable: false, mode: "repulse" },
-      onclick: { enable: false, mode: "push" },
-      resize: true
-    },
     modes: {
       grab: { distance: 400, line_linked: { opacity: 1 } },
       bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
@@ -232,8 +225,7 @@ particlesJS("particles-js", {
       push: { particles_nb: 4 },
       remove: { particles_nb: 2 }
     }
-  },
-  retina_detect: true
+  }
 });
 
 //for ajax
