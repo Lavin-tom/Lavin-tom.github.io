799122b7d0f980643ada357e06f5ace73da54afa
Lavin-tom <lavintom007@gmail.com>
2023-05-18 18:25:51 +0530

    seperate messages from main js file

--- myscript.js
+++ myscript.js
@@ -133,42 +133,19 @@ if (eventday) {
   }());
 }
 
-if (eventday) {// Define an array of messages
-  var messages = [
-    "Wishing you a very happy birthday filled with all the things you love the most. Have a great one!",
-    "Another year older, another year wiser. Happy birthday and cheers to all the great things to come!",
-    "May your birthday be as special as you are and may all your dreams come true. Enjoy your day to the fullest!",
-    "Sending you warm wishes on your birthday and hoping that this year brings you everything you've been hoping for.",
-    "Happy birthday to an amazing friend! May your day be filled with laughter, love, and all the people who make you happy",
-    "Wishing you a day filled with happiness, love, and all the things that bring a smile to your face. Happy birthday!",
-    "Here's to another year of adventures, laughs, and making memories together. Happy birthday to an awesome person!",
-    "You're not getting older, you're getting better. Happy birthday and here's to a year of growth, success, and happiness.",
-    "I hope your birthday is filled with sunshine, rainbows, and all the good things that life has to offer. Enjoy your special day!",
-    "Happy birthday to a wonderful person who deserves nothing but the best in life. May your day be filled with joy, laughter, and love. You're truly one of a kind, and I feel lucky to know you.",
-    "On your special day, I wish you all the happiness in the world. May your dreams come true and your heart be filled with love and laughter. Cheers to a new year of adventures and opportunities!",
-    "Wishing you a very happy birthday filled with love, laughter, and lots of cake! May this year bring you new adventures, opportunities, and growth. Cheers to another year of making amazing memories!"
-  ];
+// Generate a random message based on event
+if(eventday)
+{
+  var randomIndex = Math.floor(Math.random() * birthdayMessages.length);
+  // Get the message at the random index
+  var message = birthdayMessages[randomIndex];
 }
-else {
-  var messages = [
-    "In the end, everything will be okay. If it's not okay, it's not yet the end. - John Lennon",
-    "Life is 10% what happens to you and 90% how you react to it. - Charles R. Swindoll",
-    "The only way to do great work is to love what you do. - Steve Jobs",
-    "Happiness is not something ready made. It comes from your own actions. - Dalai Lama",
-    "Believe you can and you're halfway there. - Theodore Roosevelt",
-    "The best way to predict the future is to create it. - Abraham Lincoln",
-    "In three words I can sum up everything I've learned about life: it goes on. - Robert Frost",
-    "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough. - Oprah Winfrey",
-    "Be the change you wish to see in the world. - Mahatma Gandhi",
-    "Life is not about waiting for the storm to pass, it's about learning to dance in the rain. - Vivian Greene",
-    "To err is human; to forgive, Divine. - Alexander Pope"
-  ];
+else
+{
+  var randomIndex = Math.floor(Math.random() * generalMessages.length);
+  // Get the message at the random index
+  var message = generalMessages[randomIndex];
 }
-// Generate a random index
-var randomIndex = Math.floor(Math.random() * messages.length);
-
-// Get the message at the random index
-var message = messages[randomIndex];
 
 // Call the second typing function here
 var typedName = new Typed('#rand-msg', {
